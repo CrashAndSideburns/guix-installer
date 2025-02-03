58e9bdd46e30d6033fa15ca221c1895832a73d37
Morgan Arnold <morgan.arnold@proton.me>
2025-02-03 23:55:27 +0100

    actually this time

--- guix/installer.scm
+++ guix/installer.scm
@@ -27,6 +27,7 @@
   #:use-module (gnu packages vim)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages file-systems)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages mtools)
   #:use-module (gnu packages package-management)
